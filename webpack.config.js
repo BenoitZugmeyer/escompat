5ae85a2f34ccf831d3e0a00d93e27cfb26396bd3
Benoît Zugmeyer <bzugmeyer@gmail.com>
2015-05-14 17:56:45 +0200

    Optimize modules order

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ let config = {
 
 if (process.env.NODE_ENV === "production") {
   config.plugins.push(
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
