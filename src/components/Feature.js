cd88f61e69dcfbdbff7b7cb278a72f3d3901da83
Benoît Zugmeyer <bzugmeyer@gmail.com>
2015-05-14 22:46:27 +0200

    Display results

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,16 +1,64 @@
 let React = require("react");
 let FeatureClass = require("../Feature");
-let Test = require("./Test");
 let Component = require("../Component");
+
+function mixColors(c1, c2, p) {
+  return `hsl(
+    ${c1[0] * p + c2[0] * (1 - p)},
+    ${c1[1] * p + c2[1] * (1 - p)}%,
+    ${c1[2] * p + c2[2] * (1 - p)}%
+  )`;
+}
+
+function percent(n) {
+  return Math.round(n * 100) + "%";
+}
 
 class Feature extends Component {
 
   render() {
     let feature = this.props.feature;
+    let groupStyle = {
+      color: "#888",
+    };
+    let supports = [];
+    let previousSupport;
+    for (let support of feature.supports) {
+      if (!previousSupport ||
+          previousSupport.browser.shortId !== support.browser.shortId ||
+          previousSupport.score !== support.score ||
+          previousSupport.optionalScore !== support.optionalScore) {
+        supports.push(support);
+      }
+
+      previousSupport = support;
+    }
+
     return <div>
-      {feature.group} {feature.name}
-      {feature.tests.map(test => <Test key={test.name} test={test} />)}
+      <span style={groupStyle}>{feature.group}</span> {feature.name}
+      <ul>
+        {supports.map(s => this.renderSupport(s))}
+      </ul>
     </div>;
+  }
+
+  renderSupport(support) {
+    let supported = [89, 56, 62];
+    let notSupported = [10, 97, 65];
+
+    let style = {
+      backgroundColor: mixColors(supported, notSupported, support.score),
+    };
+
+    let optionalStyle = {
+      backgroundColor: mixColors(supported, notSupported, support.optionalScore),
+    };
+
+    return <li key={support.browser.id} style={style}>
+      {support.browser.short}: {percent(support.score)}
+      &nbsp;
+      {support.score !== support.optionalScore && <span style={optionalStyle}>({percent(support.optionalScore)})</span>}
+    </li>;
   }
 
 }
